c1f66693cfb4ebcbd90a05c1dcf411d4b01577c4
Maksym-Zhuravel <allwegotizus1@gmail.com>
2023-04-26 23:15:11 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+}
 
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input', debounce(onInputCountryName, DEBOUNCE_DELAY))
+
+
+function onInputCountryName() {
+    let countryName = refs.input.value.trim()
+
+
+    if (countryName === "") {
+        return
+    }
+
+    fetchCountries(countryName).then(country => {
+        if (country.length > 10) {
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        } else if (country.length >= 2 && country.length < 10) {
+            onCountryList(country)
+        }
+    })
+}
+
+
+function onCountryList(country) {
+    const countryCard = country.map(({ flags }) => `<img src="${flags.svg}" alt = "Flag"`).join('')
+
+    refs.countryInfo.innerHTML = countryCard
+
+    console.log(countryCard)
+}
